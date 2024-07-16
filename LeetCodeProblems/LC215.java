848e4b334492be4d3ab094a5e765d6bbe47f56c2
Ritik12062002 <153264986+Ritik12062002@users.noreply.github.com>
2024-07-16 04:47:15 +0000

    container congig done

---
+++ LeetCodeProblems/LC215.java
@@ -0,0 +1,19 @@
+// kth largest element
+
+class Solution {
+  public int findKthLargest(int[] nums, int k) {
+      PriorityQueue<Integer> pq = new  PriorityQueue<>(k+1);
+
+      for(int i: nums){
+          pq.add(i);
+          if(pq.size() > k){
+              pq.poll();
+          }
+      }
+      return pq.poll();
+  }
+}
+
+public class heapssss {
+
+}
