81372b311e1fa9e62d6fb0cba208c6458d06ccd2
Ritik12062002 <153264986+Ritik12062002@users.noreply.github.com>
2024-07-21 07:20:44 +0000

    Kth Missing Positive Number

---
+++ LeetCodeProblems/LC1539.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n  =arr.length;
+        int  low =0 , high =n-1;
+         
+        while (low<=high) {
+            int mid =(low+high)/2;
+            int Missing = arr[mid] - (mid+1);
+            if(Missing <k)  low =mid+1;
+            else high=mid-1;
+
+        }
+        return low+k;
+
+    }
+}
