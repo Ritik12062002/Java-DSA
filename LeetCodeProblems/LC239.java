775efae0a71c5e92af718a7fb445eb8bcf28b410
Ritik12062002 <153264986+Ritik12062002@users.noreply.github.com>
2024-07-21 07:45:02 +0000

    Sliding Window Maximum

---
+++ LeetCodeProblems/LC239.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n-k+1];
+        int ri=0;
+        Deque<Integer> q = new ArrayDeque<>();
+
+        for(int i=0;i<n;i++){
+
+            while(!q.isEmpty() && q.peek()==i-k){
+                q.poll();
+            }
+
+            while(!q.isEmpty() && nums[q.peekLast()] <= nums[i]){
+                q.pollLast();
+            }
+            q.offer(i);
+
+            if(i>=k-1){
+                res[ri++]=nums[q.peek()];
+            }
+        }
+        return res;
+    }
+}
