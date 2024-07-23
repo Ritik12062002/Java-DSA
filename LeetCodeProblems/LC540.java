4d29d0061a53d809dc20d6e3c5f25d62606658c3
Ritik12062002 <153264986+Ritik12062002@users.noreply.github.com>
2024-07-23 08:19:40 +0000

    Single Element in a Sorted Array

---
+++ LeetCodeProblems/LC540.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n =nums.length;
+        int low=0 , high =n-1;
+
+        while(low<high){
+            int mid =(low+high) /2;
+
+            if(mid%2==0 && nums[mid]==nums[mid+1] ||(mid%2 ==1 && nums[mid]== nums[mid-1])) {
+                low = mid+1;
+            } else{
+                high =mid;
+            }
+            
+        }
+         return nums[low];
+    }
+}
